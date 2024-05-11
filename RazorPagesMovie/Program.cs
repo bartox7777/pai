6e4480a5b43b52d8895fb14fa53cf4d192c0e52f
bartox7777 <bartosz.zelek@protonmail.com>
2024-05-12 01:53:44 +0200

    razor pages

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+// use SQLite in development and SQL Server in production
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext")));
+}
+else
+{
+    builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMovieContext")));
+}
+
+var app = builder.Build();
+
+// get the database context and pass it to the SeedData class
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // use the error page
+    app.UseExceptionHandler("/Error");
+
+    // force the use of HTTPS
+    app.UseHsts();
+}
+
+// redirect http to https
+app.UseHttpsRedirection();
+
+// html, css, js, images, etc.
+app.UseStaticFiles();
+
+// adds route matching to the middleware pipeline
+app.UseRouting();
+
+// configure the endpoints
+app.MapRazorPages();
+
+// run the application
+app.Run();
